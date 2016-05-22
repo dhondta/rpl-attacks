f30591f5461e1ba399ee0b8ceeb16effac3519c9
Alexandre D'Hondt <alexandre.dhondt@gmail.com>
2016-05-22 20:05:32 +0200

    Started to develop tests

--- tests/setup.py
+++ tests/setup.py
@@ -4,13 +4,11 @@ import os
 import unittest
 
 from lib.constants import COOJA_FOLDER, FRAMEWORK_FOLDER
-from lib.install import modify_cooja, update_cooja_build, update_cooja_user_properties
 
 
 class TestFabSetup(unittest.TestCase):
 
     def test_modify_cooja(self):
-        modify_cooja(COOJA_FOLDER)
         with open(os.path.join(COOJA_FOLDER, 'java', 'org', 'contikios', 'cooja', 'Cooja.java')) as f:
             source = f.read()
         for line in source.split('\n'):
@@ -19,4 +17,4 @@ class TestFabSetup(unittest.TestCase):
         self.assertRegex('-test', line)
 
     def test_update_cooja_build(self):
-        update_cooja_build(COOJA_FOLDER)
+        pass
