77cd3f562c5820a4d7a009d40c16b3ce0c3262cc
Alexandre D'Hondt <alexandre.dhondt@gmail.com>
2016-05-22 20:05:47 +0200

    Refined fabfile

--- fabfile.py
+++ fabfile.py
@@ -2,31 +2,6 @@
 # -*- coding: utf-8 -*-
 from lib import *
 
-# Note: Cooja simulation file must be the last key in the following ordered dictionary
-TEMPLATES = OrderedDict([
-    ("motes/root.c", {}),
-    ("motes/sensor.c", {}),
-    ("motes/malicious.c", {}),
-    ("Makefile", {"contiki": CONTIKI_FOLDER}),
-    ("script.js", {"timeout": 300}),  # seconds
-    ("simulation.csc", {
-        "title": "Simulation",
-        "goal": "Not specified",
-        "notes": "",
-        "target": "Z1",
-        "random_seed": "generate",
-        "transmitting_range": MAX_DIST_BETWEEN_MOTES,
-        "interference_range": 2 * MAX_DIST_BETWEEN_MOTES,
-        "success_ratio_tx": 1.0,
-        "success_ratio_rx": 1.0,
-        "mote_types": [
-            {"name": "root", "description": "DODAG root"},
-            {"name": "sensor", "description": "Normal sensor"},
-            {"name": "malicious", "description": "Malicious node"},
-        ],
-    }),
-])
-
 
 # ****************************** TASKS ON INDIVIDUAL EXPERIMENT ******************************
 @task
@@ -50,18 +25,23 @@ def cooja(name, with_malicious=False):
 @task
 def make(name, **kwargs):
     global reuse_bin_path
+    logging.info("CREATING EXPERIMENT '{}'".format(name))
+    logging.debug(" > Validating parameters...")
     params = validated_parameters(kwargs)
+    ext_lib = params.get("ext_lib")
     logging.debug(" > Creating simulation...")
     # create experiment's directories
     path = get_path(EXPERIMENT_FOLDER, name)
     get_path(EXPERIMENT_FOLDER, name, 'motes')
+    get_path(EXPERIMENT_FOLDER, name, 'data')
+    get_path(EXPERIMENT_FOLDER, name, 'results')
     # select the right malicious mote template and duplicate the simulation file
     copy_files(TEMPLATES_FOLDER, TEMPLATES_FOLDER,
                ('motes/malicious-{}.c'.format(params["mtype"]), 'motes/malicious.c'),
                ('simulation.csc', 'simulation_without_malicious.csc'),
                ('simulation.csc', 'simulation_with_malicious.csc'))
     # create experiment's files from templates
-    render_templates(path, TEMPLATES, **params)
+    render_templates(path, **params)
     # then clean the templates folder from previously created files
     remove_files(TEMPLATES_FOLDER,
                  'motes/malicious.c',
@@ -69,7 +49,7 @@ def make(name, **kwargs):
                  'simulation_with_malicious.csc')
     logging.debug(" > Making motes...")
     path = get_path(EXPERIMENT_FOLDER, name)
-    if ext_lib and not os.path.exists(ext_lib):
+    if ext_lib and not exists(ext_lib):
         logging.error("External library does not exist !")
         logging.critical("Make aborded.")
         return
@@ -88,7 +68,7 @@ def make(name, **kwargs):
                              'symbols.h',
                              'motes/root.c',
                              'motes/sensor.c')
-                remove_folder(os.path.join(path, 'obj_{}'.format(params["target"])))
+                remove_folder((path, 'obj_{}'.format(params["target"])))
                 reuse_bin_path = path
             # otherwise, reuse them by copying the compiled files to the current experiment folder
             else:
@@ -99,8 +79,8 @@ def make(name, **kwargs):
             if ext_lib is not None:
                 # backup original RPL library and replace it with attack's library
                 get_path('.tmp')
-                move_folder(os.path.join(CONTIKI_FOLDER, 'core', 'net', 'rpl'), '.tmp')
-                copy_folder(ext_lib, os.path.join(CONTIKI_FOLDER, 'core', 'net', 'rpl'))
+                move_folder((CONTIKI_FOLDER, 'core', 'net', 'rpl'), '.tmp')
+                copy_folder(ext_lib, (CONTIKI_FOLDER, 'core', 'net', 'rpl'))
             with settings(warn_only=True, abort_exception=Exception):
                 try:
                     local("make motes/malicious TARGET={}".format(params["target"]))
@@ -108,8 +88,8 @@ def make(name, **kwargs):
                     logging.error(str(e))
             if ext_lib is not None:
                 # then, clean temporary files
-                remove_folder(os.path.join(CONTIKI_FOLDER, 'core', 'net', 'rpl'))
-                move_folder('.tmp/rpl', os.path.join(CONTIKI_FOLDER, 'core', 'net'))
+                remove_folder((CONTIKI_FOLDER, 'core', 'net', 'rpl'))
+                move_folder('.tmp/rpl', (CONTIKI_FOLDER, 'core', 'net'))
                 remove_folder('.tmp')
             remove_files(path,
                          'contiki-{}.a'.format(params["target"]),
@@ -117,30 +97,16 @@ def make(name, **kwargs):
                          'symbols.c',
                          'symbols.h',
                          'motes/malicious.c')
-            remove_folder(os.path.join(path, 'obj_{}'.format(params["target"])))
+            remove_folder((path, 'obj_{}'.format(params["target"])))
         with lcd(COOJA_FOLDER):
             local("ant clean")
             local("ant jar")
-
-
-# ****************************** TASKS ON SIMULATION CAMPAIGN ******************************
-@task
-def make_all(exp_file="templates/experiments"):
-    global reuse_bin_path
-    reuse_bin_path = None
-    for name, params in get_experiments(exp_file).items():
-        logging.info("CREATING EXPERIMENT '{}'".format(name))
-        clean(name)
-        make(name, **validated_parameters(params))
-
 
 @task
 def run(name):
     logging.info("PROCESSING EXPERIMENT '{}'".format(name))
     path = get_path(EXPERIMENT_FOLDER, name)
     with hide(*HIDDEN_ALL):
-        get_path(EXPERIMENT_FOLDER, name, 'data')
-        get_path(EXPERIMENT_FOLDER, name, 'results')
         with lcd(path):
             logging.debug(" > Running both simulations (with and without the malicious mote)...")
             local("make run-without-malicious")
@@ -150,8 +116,26 @@ def run(name):
                      'COOJA.testlog')
 
 
+# ****************************** TASKS ON SIMULATION CAMPAIGN ******************************
 @task
-def run_all(exp_file="templates/experiments"):
+@expand_file(TEMPLATES_FOLDER, 'json')
+def make_all(exp_file="experiments"):
+    global reuse_bin_path
+    for name, params in get_experiments(exp_file).items():
+        clean(name)
+        make(name, **params)
+
+
+@task
+@expand_file(EXPERIMENT_FOLDER, 'json')
+def prepare(exp_file='my_simulation'):
+    logging.debug("CREATING NEW EXPERIMENT CAMPAIGN AT '{}'".format(exp_file))
+    copy_files(TEMPLATES_FOLDER, dirname(exp_file), ('experiments.json', exp_file))
+
+
+@task
+@expand_file(TEMPLATES_FOLDER, 'json')
+def run_all(exp_file="experiments"):
     for name in get_experiments(exp_file).keys():
         run(name)
 
@@ -160,18 +144,18 @@ def run_all(exp_file="templates/experiments"):
 @task
 def config(contiki_folder='~/contiki', experiments_folder='~/Experiments'):
     logging.debug("CREATING CONFIGURATION FILE AT '~/.rpl-attacks.conf'")
-    with open(os.path.expanduser('~/.rpl-attacks.conf'), 'w') as f:
+    with open(expanduser('~/.rpl-attacks.conf'), 'w') as f:
         f.write('[RPL Attacks Framework Configuration]\n')
         f.write('contiki_folder = {}\n'.format(contiki_folder))
         f.write('experiments_folder = {}\n'.format(experiments_folder))
 
 
 @task
-def prepare(exp_file='my_simulation'):
-    if not exp_file.endswith('.json'):
-        exp_file += '.json'
-    logging.debug("CREATING NEW EXPERIMENT CAMPAIGN AT '{}'".format(os.path.join(EXPERIMENT_FOLDER, exp_file)))
-    copy_files(TEMPLATES_FOLDER, EXPERIMENT_FOLDER, ('experiments.json', exp_file))
+def test():
+    setup()
+    make("test-simulation")
+    with lcd(FRAMEWORK_FOLDER):
+        local("python -m unittest tests")
 
 
 @task
@@ -189,10 +173,17 @@ def setup():
         modify_cooja(COOJA_FOLDER)
         update_cooja_build(COOJA_FOLDER)
         update_cooja_user_properties()
-        visualizer = os.path.join(COOJA_FOLDER, 'apps', 'visualizer_screenshot')
-        if not os.path.exists(visualizer):
+        visualizer = join(COOJA_FOLDER, 'apps', 'visualizer_screenshot')
+        if not exists(visualizer):
             copy_folder('src/visualizer_screenshot', visualizer)
         with lcd(COOJA_FOLDER):
             with settings(warn_only=True):
                 local("ant clean")
             local("ant jar")
+
+
+# **************************************** MAGIC TASK ****************************************
+@task
+def rip_my_slip():
+    # TODO: prepare 'rpl-attacks' campaign, make all its experiments then run them
+    print("yeah, man !")
