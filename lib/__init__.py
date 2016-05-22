5856017fbb7331cdaad100e1efb1f546c3b47c56
Alexandre D'Hondt <alexandre.dhondt@gmail.com>
2016-05-22 13:36:58 +0200

    Refactored lib package

--- lib/__init__.py
+++ lib/__init__.py
@@ -8,8 +8,8 @@ from fabric.api import hide, lcd, local, settings, task
 from .analyze import dashboard
 from .constants import CONTIKI_FOLDER, COOJA_FOLDER, EXPERIMENT_FOLDER, TEMPLATES_FOLDER, TEMPLATE_ENV, \
                        MAX_XY_POSITION, MIN_DIST_BETWEEN_MOTES, MAX_DIST_BETWEEN_MOTES, NBR_MOTES
-from .helpers import copy_files, copy_folder, move_folder, remove_files, remove_folder, render_templates
+from .helpers import copy_files, copy_folder, move_folder, remove_files, remove_folder
 from .install import modify_cooja, update_cooja_build, update_cooja_user_properties
-from .logconfig import HIDDEN
+from .logconfig import HIDDEN_ALL, HIDDEN_KEEP_STDERR
 from .parser import powertracker2csv, pcap2csv
-from .utils import get_experiments, get_path
+from .utils import get_experiments, get_path, render_templates, validated_parameters
