5048cda4670124e76aff5bce231604ac8136d6f0
Alexandre D'Hondt <alexandre.dhondt@gmail.com>
2016-05-23 19:55:06 +0200

    Improved commands

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,7 @@ from lib import *
 
 # ****************************** TASKS ON INDIVIDUAL EXPERIMENT ******************************
 @task
+@report_bad_input
 def clean(name):
     logging.debug(" > Cleaning folder...")
     with hide(*HIDDEN_ALL):
@@ -13,6 +14,7 @@ def clean(name):
 
 
 @task
+@report_bad_input
 def cooja(name, with_malicious=False):
     logging.info("STARTING COOJA WITH EXPERIMENT '{}'".format(name))
     path = get_path(EXPERIMENT_FOLDER, name)
@@ -23,6 +25,7 @@ def cooja(name, with_malicious=False):
 
 
 @task
+@report_bad_input
 def make(name, **kwargs):
     global reuse_bin_path
     logging.info("CREATING EXPERIMENT '{}'".format(name))
@@ -47,19 +50,20 @@ def make(name, **kwargs):
                  'motes/malicious.c',
                  'simulation_without_malicious.csc',
                  'simulation_with_malicious.csc')
-    logging.debug(" > Making motes...")
     path = get_path(EXPERIMENT_FOLDER, name)
     if ext_lib and not exists(ext_lib):
         logging.error("External library does not exist !")
         logging.critical("Make aborded.")
-        return
-    with hide(*HIDDEN_ALL):
+        exit(2)
+    with settings(hide(*HIDDEN_ALL), warn_only=True):
         with lcd(path):
             # for every simulation, root and sensor compilation is the same ;
             #  then, if these were not previously compiled, proceed
             if reuse_bin_path is None:
-                local("make motes/root TARGET={}".format(params["target"]))
-                local("make motes/sensor TARGET={}".format(params["target"]))
+                logging.debug(" > Making 'root.{}'...".format(params["target"]))
+                stderr(local)("make motes/root TARGET={}".format(params["target"]), capture=True)
+                logging.debug(" > Making 'sensor.{}'...".format(params["target"]))
+                stderr(local)("make motes/sensor TARGET={}".format(params["target"]), capture=True)
                 # after compiling, clean artifacts
                 remove_files(path,
                              'contiki-{}.a'.format(params["target"]),
@@ -74,18 +78,14 @@ def make(name, **kwargs):
             else:
                 copy_files(reuse_bin_path, path, "motes/root.{}".format(params["target"]),
                            "motes/sensor.{}".format(params["target"]))
-    with hide(*HIDDEN_KEEP_STDERR):
-        with lcd(path):
+            # now, handle the malicious mote compilation
             if ext_lib is not None:
                 # backup original RPL library and replace it with attack's library
                 get_path('.tmp')
                 move_folder((CONTIKI_FOLDER, 'core', 'net', 'rpl'), '.tmp')
                 copy_folder(ext_lib, (CONTIKI_FOLDER, 'core', 'net', 'rpl'))
-            with settings(warn_only=True, abort_exception=Exception):
-                try:
-                    local("make motes/malicious TARGET={}".format(params["target"]))
-                except Exception as e:
-                    logging.error(str(e))
+            logging.debug(" > Making 'malicious.{}'...".format(params["target"]))
+            stderr(local)("make motes/malicious TARGET={}".format(params["target"]), capture=True)
             if ext_lib is not None:
                 # then, clean temporary files
                 remove_folder((CONTIKI_FOLDER, 'core', 'net', 'rpl'))
@@ -103,6 +103,7 @@ def make(name, **kwargs):
             local("ant jar")
 
 @task
+@report_bad_input
 def run(name):
     logging.info("PROCESSING EXPERIMENT '{}'".format(name))
     path = get_path(EXPERIMENT_FOLDER, name)
@@ -117,7 +118,7 @@ def run(name):
 
 
 # ****************************** TASKS ON SIMULATION CAMPAIGN ******************************
-@task
+@task(alias='make-all')
 @expand_file(EXPERIMENT_FOLDER, 'json')
 def make_all(exp_file="experiments"):
     global reuse_bin_path
@@ -133,7 +134,7 @@ def prepare(exp_file='my_simulation'):
     copy_files(TEMPLATES_FOLDER, dirname(exp_file), ('experiments.json', exp_file))
 
 
-@task
+@task(alias='run-all')
 @expand_file(EXPERIMENT_FOLDER, 'json')
 def run_all(exp_file="experiments"):
     for name in get_experiments(exp_file).keys():
@@ -160,35 +161,35 @@ def test():
 
 @task
 def setup():
-    # Install
-    cooja_addons_installed = False
-    try:
-        with open('.cooja_addons_installed') as f:
-            f.read()
-        logging.info("COOJA ADD-ONS ALREADY INSTALLED")
-        cooja_addons_installed = True
-    except IOError:
-        with open('.cooja_addons_installed', 'w') as f:
-            f.write("")
-    if not cooja_addons_installed:
+    # install Cooja modifications
+    if not check_cooja(COOJA_FOLDER):
         logging.info("INSTALLING COOJA ADD-ONS")
-        with hide(*HIDDEN_ALL):
-            modify_cooja(COOJA_FOLDER)
-            update_cooja_build(COOJA_FOLDER)
-            update_cooja_user_properties()
-            visualizer = join(COOJA_FOLDER, 'apps', 'visualizer_screenshot')
-            if not exists(visualizer):
-                copy_folder('src/visualizer_screenshot', visualizer)
-            with lcd(COOJA_FOLDER):
-                with settings(warn_only=True):
-                    local("ant clean")
-                local("ant jar")
+        # modify Cooja.java and adapt build.xml and ~/.cooja.user.properties
+        modify_cooja(COOJA_FOLDER)
+        update_cooja_build(COOJA_FOLDER)
+        update_cooja_user_properties()
+        # install VisualizerScreenshot plugin in Cooja
+        visualizer = join(COOJA_FOLDER, 'apps', 'visualizer_screenshot')
+        if not exists(visualizer):
+            logging.debug(" > Installing VisualizerScreenshot Cooja plugin...")
+            copy_folder('src/visualizer_screenshot', visualizer)
+        # recompile Cooja for making the changes take effect
+        with lcd(COOJA_FOLDER):
+            logging.debug(" > Recompiling Cooja...")
+            with settings(warn_only=True):
+                local("ant clean")
+            local("ant jar")
+    else:
+        logging.info("COOJA IS UP-TO-DATE")
+    # install msp430 (GCC) upgrade
     with hide(*HIDDEN_ALL):
         msp430_version_output = local('msp430-gcc --version', capture=True)
     if 'msp430-gcc (GCC) 4.7.0 20120322' not in msp430_version_output:
         if input("In order to extend msp430x memory support, it is necessary to upgrade msp430-gcc.\n"
                  "Would you like to upgrade it now ? (yes|no) [default: no] ") == "yes":
             logging.info("UPGRADING msp430-gcc FROM VERSION 4.6.3 TO 4.7.0")
+            logging.warning("If you encounter problems with this upgrade, please refer to:\n"
+                            "https://github.com/contiki-os/contiki/wiki/MSP430X")
             with lcd('src/'):
                 logging.warning(" > Upgrade now starts, this may take up to 30 minutes...")
                 sudo('./upgrade-msp430.sh')
@@ -203,7 +204,7 @@ def setup():
 
 
 # **************************************** MAGIC TASK ****************************************
-@task
+@task(alias='rip-my-slip', default=True)
 def rip_my_slip():
     # TODO: prepare 'rpl-attacks' campaign, make all its experiments then run them
     print("yeah, man !")
