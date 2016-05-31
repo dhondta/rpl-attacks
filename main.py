87507307f113f3f254cd7f26108039eccf27b585
Alexandre D'Hondt <alexandre.dhondt@gmail.com>
2016-05-31 17:01:59 +0200

    Updated main

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from os import system
-
 from lib.console import FrameworkConsole
 
 
 if __name__ == '__main__':
-    system('clear')
-    cli = FrameworkConsole(parallel=True,
-                           disabled=['@@', 'ed', 'hi', 'l', 'li', 'pause', 'q', 'r',
-                                     'cmdenvironment', 'save', '_load', '_relative_load'])
-    cli.cmdloop()
+    FrameworkConsole(parallel=True).cmdloop()
